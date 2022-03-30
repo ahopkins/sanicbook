b604e601e285cec09b553513fd325cd80dc20504
Adam Hopkins <adam@amhopkins.com>
2022-03-31 00:16:02 +0300

    Add HTTP redirect

--- application/booktracker/middleware/redirect.py
+++ application/booktracker/middleware/redirect.py
@@ -1,10 +1,13 @@
 from sanic import Request, Sanic
+from sanic.response import redirect
 
 app = Sanic.get_app("BooktrackerApp")
 
 
 @app.on_request
 async def https_redirect(request: Request):
-    for key, value in request.headers.items():
-        if key not in ("authorization", "cookie"):
-            print(key, value)
+    if (
+        request.forwarded.get("x-forwarded-proto") == "http"
+        and not app.config.LOCAL
+    ):
+        redirect(request.url.replace("http://", "https://"))
